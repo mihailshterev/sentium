51127dc2d80868152753c53307b67d015ff87626
Shredrox <93082689+Shredrox@users.noreply.github.com>
2026-01-18 10:21:16 +0200

    remove unneeded class

--- services/IdentityProvider/IdentityProvider.Application/Options/JwtOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityProvider.Application.Options;
-
-public sealed class JwtOptions
-{
-    public string Secret { get; set; } = null!;
-    public string Issuer { get; set; } = null!;
-    public string Audience { get; set; } = null!;
-    public int ExpiryMinutes { get; set; }
-}
--- services/IdentityProvider/IdentityProvider.Application/Services/AuthService.cs
+++ services/IdentityProvider/IdentityProvider.Application/Services/AuthService.cs
@@ -1,45 +1,7 @@
-using IdentityProvider.Application.Options;
-using IdentityProvider.Core.Users;
-using IdentityProvider.Infrastructure.Identity;
-using IdentityProvider.Infrastructure.Mappings;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-
 namespace IdentityProvider.Application.Services;
 
-public sealed class AuthenticationService(UserManager<ApplicationUser> userManager, IOptions<JwtOptions> jwtOptions)
+public sealed class AuthService()
 {
-    private readonly JwtOptions _jwtOptions = jwtOptions.Value;
 
-    public async Task<string> GenerateJwtAsync(SystemUser user)
-    {
-        var appUser = user.ToApplicationUser();
-        var roles = await userManager.GetRolesAsync(appUser);
-
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.NameIdentifier, appUser.Id.ToString()),
-            new Claim(ClaimTypes.Email, appUser.Email!)
-        };
-
-        claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Secret));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        var token = new JwtSecurityToken(
-            issuer: _jwtOptions.Issuer,
-            audience: _jwtOptions.Audience,
-            claims: claims,
-            expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpiryMinutes),
-            signingCredentials: creds
-        );
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
-    }
 }
 
