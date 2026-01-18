095a31ea53243cc589d9ff899a5c642cdf9cacc0
Shredrox <93082689+Shredrox@users.noreply.github.com>
2026-01-18 11:04:44 +0200

    update auth setup

---
+++ services/IdentityProvider/IdentityProvider.Core/Security/Scopes.cs
@@ -0,0 +1,10 @@
+namespace IdentityProvider.Core.Security;
+
+public static class Scopes
+{
+    public const string Api = "api";
+    public const string Roles = "roles";
+    public const string OpenId = "openid";
+    public const string Profile = "profile";
+    public const string Email = "email";
+}
--- services/IdentityProvider/IdentityProvider.Infrastructure/ServiceCollectionExtensions.cs
+++ services/IdentityProvider/IdentityProvider.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,16 +1,12 @@
-using System.Text;
+using IdentityProvider.Core.Security;
 using IdentityProvider.Infrastructure.Data;
 using IdentityProvider.Infrastructure.Identity;
 using IdentityProvider.Infrastructure.Identity.OpenIddict;
-using IdentityProvider.Infrastructure.Options;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityProvider.Infrastructure;
 
@@ -31,45 +27,6 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddIdentityServices(this IServiceCollection services)
     {
-        var jwtOptions = services.BuildServiceProvider().GetRequiredService<IOptionsSnapshot<JwtOptions>>().Value;
-
-        services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(options =>
-        {
-            options.SaveToken = true;
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret)),
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = jwtOptions.Issuer,
-                ValidAudience = jwtOptions.Audience,
-                ClockSkew = TimeSpan.Zero
-            };
-
-            options.Events = new JwtBearerEvents
-            {
-                OnMessageReceived = context =>
-                {
-                    if (context.Request.Cookies.ContainsKey("AccessToken"))
-                    {
-                        context.Token = context.Request.Cookies["AccessToken"];
-                    }
-
-                    return Task.CompletedTask;
-                }
-            };
-        });
-
-        services.AddAuthorizationBuilder();
-
         services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
         {
             options.Password.RequireDigit = true;
@@ -81,26 +38,34 @@ public static class ServiceCollectionExtensions
         .AddEntityFrameworkStores<IdentityDbContext>()
         .AddDefaultTokenProviders();
 
-        services.AddOpenIddict().AddCore(options =>
-        {
-            options.UseEntityFrameworkCore()
-                   .UseDbContext<IdentityDbContext>();
-        }).AddServer(options =>
-        {
-            options.AllowAuthorizationCodeFlow();
+        services.AddOpenIddict()
+            .AddCore(options =>
+            {
+                options.UseEntityFrameworkCore()
+                       .UseDbContext<IdentityDbContext>();
+            })
+            .AddServer(options =>
+            {
+                options.AllowAuthorizationCodeFlow();
 
-            options.AddDevelopmentEncryptionCertificate()
-                    .AddDevelopmentSigningCertificate();
+                options.AddDevelopmentEncryptionCertificate()
+                        .AddDevelopmentSigningCertificate();
 
-            options.UseAspNetCore()
-                   .EnableAuthorizationEndpointPassthrough()
-                   .EnableTokenEndpointPassthrough();
+                options.UseAspNetCore()
+                       .EnableAuthorizationEndpointPassthrough()
+                       .EnableTokenEndpointPassthrough();
 
-            options.SetAuthorizationEndpointUris("/connect/authorize");
-            options.SetTokenEndpointUris("/connect/token");
+                options.SetAuthorizationEndpointUris("/connect/authorize");
+                options.SetTokenEndpointUris("/connect/token");
 
-            options.RegisterScopes("openid", "profile", "email", "api");
-        });
+                options.RegisterScopes(
+                    Scopes.OpenId,
+                    Scopes.Profile,
+                    Scopes.Email,
+                    Scopes.Api,
+                    Scopes.Roles
+                );
+            });
 
         return services;
     }
