5e136bf23562adcbb379c74d750f6a318807530a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2026-01-24 21:53:27 +0200

    add Sentinel project

--- aspire/Program.cs
+++ aspire/Program.cs
@@ -2,5 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.ApiGateway>("api-gateway");
 builder.AddProject<Projects.IdentityProvider_Api>("identity-provider");
+builder.AddProject<Projects.Sentinel_Api>("sentinel");
 
 builder.Build().Run();
