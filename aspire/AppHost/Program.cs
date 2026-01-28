d47b5c4ea8698251da1c1f5b9f78c7ac63b327b3
Shredrox <93082689+Shredrox@users.noreply.github.com>
2026-01-28 21:48:13 +0200

    update apphost setup

--- aspire/AppHost/Program.cs
+++ aspire/AppHost/Program.cs
@@ -1,10 +1,21 @@
+using AppHost;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var identityApi = builder.AddProject<Projects.IdentityProvider_Api>("identity-provider");
-var sentinelApi = builder.AddProject<Projects.Sentinel_Api>("sentinel");
+var ollama = builder.AddOllama(ServiceConstants.OllamaServiceName)
+    .WithGPUSupport(OllamaGpuVendor.Nvidia);
+//.WithOpenWebUI();
 
-builder.AddProject<Projects.ApiGateway>("gateway")
-    .WithReference(identityApi)
-    .WithReference(sentinelApi);
+var gemma31b = ollama.AddModel(ServiceConstants.Gemma3ModelName);
+
+var identityApi = builder.AddProject<Projects.IdentityProvider_Api>(ServiceNames.Identity);
+var sentinelApi = builder.AddProject<Projects.Sentinel_Api>(ServiceNames.Sentinel);
+var agentRuntimeApi = builder.AddProject<Projects.AgentRuntime_Api>(ServiceNames.AgentRuntime)
+    .WithReference(gemma31b).WaitFor(gemma31b);
+
+builder.AddProject<Projects.ApiGateway>(ServiceNames.Gateway)
+    .WithReference(identityApi).WaitFor(identityApi)
+    .WithReference(sentinelApi).WaitFor(sentinelApi)
+    .WithReference(agentRuntimeApi).WaitFor(agentRuntimeApi);
 
 builder.Build().Run();
