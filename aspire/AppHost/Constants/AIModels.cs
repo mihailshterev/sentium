62e9e945ab1cfdbed4ec89fe782093a6a1180546
Mihail Shterev <93082689+mihailshterev@users.noreply.github.com>
2026-03-10 11:56:55 +0200

    add Qwen3_8_Q4_K_M model

--- aspire/AppHost/Constants/AIModels.cs
+++ aspire/AppHost/Constants/AIModels.cs
@@ -5,4 +5,5 @@ public static class AIModels
     public const string Gemma3 = "gemma3:1b";
     public const string Qwen3_4 = "qwen3:4b";
     public const string Qwen3_8 = "qwen3:8b";
+    public const string Qwen3_8_Q4_K_M = "qwen3:8b-q4_K_M";
 }
