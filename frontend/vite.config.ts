82f98e1653970d3a8bae7264698100b3c6975278
Mihail Shterev <93082689+mihailshterev@users.noreply.github.com>
2026-03-14 09:51:48 +0200

    more package updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import sassDts from "vite-plugin-sass-dts";
+import babel from '@rolldown/plugin-babel'
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
     sassDts({
       enabledMode: ["development", "production"],
       global: {
