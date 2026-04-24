cdc51df77c9cd61a68957ee0f420501099752833
Mihail Shterev <93082689+mihailshterev@users.noreply.github.com>
2026-04-24 21:13:25 +0300

    register event store

--- services/Sentinel/Sentinel.Application/ServiceCollectionExtensions.cs
+++ services/Sentinel/Sentinel.Application/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Sentinel.Application.Engine;
+using Sentinel.Application.Services;
+using Sentinel.Core.Stores;
 
 namespace Sentinel.Application;
 
@@ -8,6 +10,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddSingleton<SentinelPolicyEngine>();
+        services.AddSingleton<INetworkEventStore, InMemoryNetworkEventStore>();
         return services;
     }
 }
