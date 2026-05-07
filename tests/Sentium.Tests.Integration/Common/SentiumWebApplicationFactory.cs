add28d11a461b20902cb134311baa5709cc87eee
Mihail Shterev <93082689+mihailshterev@users.noreply.github.com>
2026-05-07 16:05:45 +0300

    add seq endpoint in tests

--- tests/Sentium.Tests.Integration/Common/SentiumWebApplicationFactory.cs
+++ tests/Sentium.Tests.Integration/Common/SentiumWebApplicationFactory.cs
@@ -37,6 +37,7 @@ public class SentiumWebApplicationFactory<TProgram> : WebApplicationFactory<TPro
         builder.UseSetting($"ConnectionStrings:{ResourceNames.Nats}", "nats://localhost:4222");
         builder.UseSetting($"ConnectionStrings:{ResourceNames.Qdrant}", "Endpoint=http://localhost:6334");
         builder.UseSetting($"ConnectionStrings:{ResourceNames.WorkspaceBlobs}", "UseDevelopmentStorage=true");
+        builder.UseSetting($"ConnectionStrings:{ResourceNames.Seq}", "http://localhost:5341");
 
         builder.ConfigureTestServices(services =>
         {
